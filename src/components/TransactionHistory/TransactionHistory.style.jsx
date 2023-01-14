ff13733869e3917d5266d82d80326384e473ecb2
Belkina23 <23zakat@gmail.com>
2023-01-14 17:01:32 +0200

    sjnds

--- src/components/TransactionHistory/TransactionHistory.style.jsx
+++ src/components/TransactionHistory/TransactionHistory.style.jsx
@@ -1,9 +1,35 @@
 import styled from '@emotion/styled';
 
-const Table = styled.table``;
+const Table = styled.table`
+    background-color: #ffffff;
+    table-layout: fixed;
+    margin: 50px auto;
+    width: 500px;
+    border-collapse: collapse;
+    box-shadow: 1px 1px 4px #565656;
+    th,
+    td {
+        padding: 10px;
+        height: 20px;
+    }
+`;
 
-const Thead = styled.thead``;
+const Thead = styled.thead`
+    th {
+        text-align: center;
+        color: #fff;
+        background-color: #269768;
+    }
+`;
 
-const Tbody = styled.tbody``;
-
+const Tbody = styled.tbody`
+    tr:nth-of-type(odd) {
+        background-color: #eee;
+    }
+    td {
+        text-align: center;
+        color: #696969;
+        border-bottom: 1px solid #eee;
+    }
+`;
 export {Table, Thead, Tbody};
